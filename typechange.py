d03a309b231fb17653ee8dc4cd31d6a00e04beba
ravitejaare <39189879+ravitejaare@users.noreply.github.com>
2020-03-21 19:55:58 +0530

    Update typechange.py

--- typechange.py
+++ typechange.py
@@ -1,15 +1,15 @@
 import boto3
 ec2 = boto3.client('ec2')
 instances = ec2.describe_instances()
-echo '$InstanceId'
+echo '$InstanceID'
 for reservations in instances['Reservations']:
   for instance in reservations['Instances']:
      if(instance['State']['Name'] == 'running'):
        #ec2.stop_instances(InstanceIds=[instance['InstanceId']])
-        print("this is instanceid", '$InstanceId')
-        ec2.stop_instances(InstanceIds=['$InstanceId'])
-        ec2.modify_instance_attribute(InstanceId='$InstanceId', Attribute='instanceType', Value='$type')
-        ec2.start_instances(InstanceIds=['$InstanceId'])
+        print("this is instanceid", '$InstanceID')
+        ec2.stop_instances(InstanceIds=['$InstanceID'])
+        ec2.modify_instance_attribute(InstanceId='$InstanceID', Attribute='instanceType', Value='$type')
+        ec2.start_instances(InstanceIds=['$InstanceID'])
      else:
-       ec2.modify_instance_attribute(InstanceId='$InstanceId', Attribute='instanceType', Value='$type')
-       ec2.start_instances(InstanceIds=['$InstanceId'])
+       ec2.modify_instance_attribute(InstanceId='$InstanceID', Attribute='instanceType', Value='$type')
+       ec2.start_instances(InstanceIds=['$InstanceID'])
