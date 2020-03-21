8cc410992124562c640bd44762e0eb9ac3aaa608
ravitejaare <39189879+ravitejaare@users.noreply.github.com>
2020-03-21 18:15:05 +0530

    Create typechange.py

---
+++ typechange.py
@@ -0,0 +1,13 @@
+import boto3
+ec2 = boto3.client('ec2')
+instances = ec2.describe_instances()
+for reservations in instances['Reservations']:
+  for instance in reservations['Instances']:
+     if(instance['State']['Name'] == 'running'):
+       #ec2.stop_instances(InstanceIds=[instance['InstanceId']])
+        ec2.stop_instances(InstanceIds=[$InstanceID])
+        ec2.modify_instance_attribute(InstanceId=$InstanceID, Attribute='instanceType', Value='m3.xlarge')
+        ec2.start_instances(InstanceIds=[$InstanceID])
+     else:
+       ec2.modify_instance_attribute(InstanceId=$InstanceID, Attribute='instanceType', Value='m3.xlarge')
+       ec2.start_instances(InstanceIds=[$InstanceID])
