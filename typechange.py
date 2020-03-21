7477410e44f26c80a01f890fc312f7f1963ba198
ravitejaare <39189879+ravitejaare@users.noreply.github.com>
2020-03-21 20:22:13 +0530

    Update typechange.py

--- typechange.py
+++ typechange.py
@@ -1,16 +1,4 @@
 import boto3
-import os
 ec2 = boto3.client('ec2')
 instances = ec2.describe_instances()
-print("this is id: os.getenv("InstanceID")")
-for reservations in instances['Reservations']:
-  for instance in reservations['Instances']:
-     if(instance['State']['Name'] == 'running'):
-       #ec2.stop_instances(InstanceIds=[instance['InstanceId']])
-        print("this is instanceid", '$InstanceID')
-        ec2.stop_instances(InstanceIds=['$InstanceID'])
-        ec2.modify_instance_attribute(InstanceId='$InstanceID', Attribute='instanceType', Value='$type')
-        ec2.start_instances(InstanceIds=['$InstanceID'])
-     else:
-       ec2.modify_instance_attribute(InstanceId='$InstanceID', Attribute='instanceType', Value='$type')
-       ec2.start_instances(InstanceIds=['$InstanceID'])
+print(" Welcome to Python")
