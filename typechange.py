91da06b7f4cc300469752df5fc8390e47ac5180c
ravitejaare <39189879+ravitejaare@users.noreply.github.com>
2020-03-21 18:50:45 +0530

    Update typechange.py

--- typechange.py
+++ typechange.py
@@ -5,9 +5,9 @@ for reservations in instances['Reservations']:
   for instance in reservations['Instances']:
      if(instance['State']['Name'] == 'running'):
        #ec2.stop_instances(InstanceIds=[instance['InstanceId']])
-        ec2.stop_instances(InstanceIds=[$InstanceId])
+        ec2.stop_instances(InstanceIds=['$InstanceId'])
         ec2.modify_instance_attribute(InstanceId=$InstanceId, Attribute='instanceType', Value=$type)
-        ec2.start_instances(InstanceIds=[$InstanceId])
+        ec2.start_instances(InstanceIds=['$InstanceId'])
      else:
-       ec2.modify_instance_attribute(InstanceId=$InstanceId, Attribute='instanceType', Value=$type)
-       ec2.start_instances(InstanceIds=[$InstanceId])
+       ec2.modify_instance_attribute(InstanceId='$InstanceId', Attribute='instanceType', Value=$type)
+       ec2.start_instances(InstanceIds=['$InstanceId'])
