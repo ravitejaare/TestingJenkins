b1707c335021c977da4ad768e6e898c49fb424f6
ravitejaare <39189879+ravitejaare@users.noreply.github.com>
2020-03-21 21:07:21 +0530

    Update typechange.py

--- typechange.py
+++ typechange.py
@@ -2,18 +2,18 @@ import boto3
 import sys
 ec2 = boto3.client('ec2')
 instances = ec2.describe_instances()
-print("this is instanceid", sys.argv[0])
+print("this is instanceid", sys.argv[1])
 
 #print("this is id: os.getenv("InstanceID")")
 for reservations in instances['Reservations']:
   for instance in reservations['Instances']:
      if(instance['State']['Name'] == 'running'):
        #ec2.stop_instances(InstanceIds=[instance['InstanceId']])
-        print("this is instanceid", sys.argv[0])
-        ec2.stop_instances(InstanceIds=['sys.argv[0]'])
-        ec2.modify_instance_attribute(InstanceId='sys.argv[0]', Attribute='instanceType', Value='sys.argv[1]')
-        ec2.start_instances(InstanceIds=['sys.argv[0]'])
+        print("this is instanceid", sys.argv[2])
+        ec2.stop_instances(InstanceIds=['sys.argv[1]'])
+        ec2.modify_instance_attribute(InstanceId='sys.argv[1]', Attribute='instanceType', Value='sys.argv[2]')
+        ec2.start_instances(InstanceIds=['sys.argv[1]'])
      else:
-       ec2.modify_instance_attribute(InstanceId='sys.argv[0]', Attribute='instanceType', Value='sys.argv[1]')
-       ec2.start_instances(InstanceIds=['sys.argv[0]'])
+       ec2.modify_instance_attribute(InstanceId='sys.argv[1]', Attribute='instanceType', Value='sys.argv[2]')
+       ec2.start_instances(InstanceIds=['sys.argv[1]'])
 
