bab178423f86d902c7c2ef09cc3a1a5a5e9f1863
ravitejaare <39189879+ravitejaare@users.noreply.github.com>
2020-03-21 13:41:42 +0530

    Update vmstart.sh

--- ec2startstop/vmstart.sh
+++ ec2startstop/vmstart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ "$State" = "start" ]
 then 
-aws ec2 --region ap-south-1 start-instances --instance-ids $InstanceID
+aws ec2 --region us-east-1 start-instances --instance-ids $InstanceID
 echo Instance $InstanceID Started
 elif [ "$State" = "stop" ]
 then
-aws ec2  --region ap-south-1 stop-instances --instance-ids $InstanceID
+aws ec2  --region us-east-1 stop-instances --instance-ids $InstanceID
 echo Instance $InstanceID Stopped
  fi
