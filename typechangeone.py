ed1292225906439c787cc5e89c1b613f8bad1014
ravitejaare <39189879+ravitejaare@users.noreply.github.com>
2020-03-22 11:36:19 +0530

    Update typechangeone.py

--- typechangeone.py
+++ typechangeone.py
@@ -2,9 +2,11 @@ import boto3
 import sys
 ec2 = boto3.client('ec2',region_name='us-east-1')
 instances = ec2.describe_instances()
+print("my instance id", $InstanceID)
+print("my type is", $type)
 for reservations in instances['Reservations']:
   for instance in reservations['Instances']:
-     if((instance['State']['Name'] == 'running') and (instance['InstanceId'] == $InstanceID)):
+     if((instance['State']['Name'] == 'running') and (instance['InstanceId'] == '$InstanceID')):
        #ec2.stop_instances(InstanceIds=[instance['InstanceId']])
         print("this is instanceid", sys.argv[2])
         ec2.stop_instances(InstanceIds=[$InstanceID])
