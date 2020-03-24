6d19709d9ff745d24708d443dab4604d1fe2c048
ravitejaare <39189879+ravitejaare@users.noreply.github.com>
2020-03-24 19:05:38 +0530

    Update typechange.py

--- typechange.py
+++ typechange.py
@@ -7,22 +7,24 @@ print("the instance type",sys.argv[2])
 id= sys.argv[1]
 type= sys.argv[2]
 
-#print("this is id: os.getenv("InstanceID")")
 for reservations in instances['Reservations']:
   for instance in reservations['Instances']:
-     if((instance['State']['Name'] == 'running') and (instance['InstanceId'] ==sys.argv[1])):
-       #ec2.stop_instances(InstanceIds=[instance['InstanceId']])
-        print("this is instanceid", sys.argv[1])
+     if((instance['State']['Name'] == 'running'):
         #ec2.stop_instances(InstanceIds=[id])
         #waiter=ec2.get_waiter('instance_stopped')
         #waiter.wait(InstanceIds=[id])
-        ec2.modify_instance_attribute(InstanceId=id, Attribute='instanceType', Value=type)
-        ec2.start_instances(InstanceIds=[id])
+        ec2.stop_instances(InstanceIds=[id])
      else:
-       ec2.modify_instance_attribute(InstanceId=id, Attribute='instanceType', Value=type)
+       pass
+       #ec2.modify_instance_attribute(InstanceId=id, Attribute='instanceType', Value=type)
        #ec2.start_instances(InstanceIds=[id])
       
- 
+
+#instancemodify()
+
+#def instancemodify:
+ec2.modify_instance_attribute(InstanceId=id, Attribute='instanceType', Value=type)
+
 ec2.start_instances(InstanceIds=[id])
 
 
