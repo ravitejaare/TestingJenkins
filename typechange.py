1bba46795b4c97baa40a5d1507e25f4f616dd79b
ravitejaare <39189879+ravitejaare@users.noreply.github.com>
2020-03-24 17:49:01 +0530

    Update typechange.py

--- typechange.py
+++ typechange.py
@@ -13,9 +13,9 @@ for reservations in instances['Reservations']:
      if((instance['State']['Name'] == 'running') and (instance['InstanceId'] ==sys.argv[1])):
        #ec2.stop_instances(InstanceIds=[instance['InstanceId']])
         print("this is instanceid", sys.argv[1])
-        ec2.stop_instances(InstanceIds=[id])
-        waiter=ec2.get_waiter('instance_stopped')
-        waiter.wait(InstanceIds=[id])
+        #ec2.stop_instances(InstanceIds=[id])
+        #waiter=ec2.get_waiter('instance_stopped')
+        #waiter.wait(InstanceIds=[id])
         ec2.modify_instance_attribute(InstanceId=id, Attribute='instanceType', Value=type)
         ec2.start_instances(InstanceIds=[id])
      else:
